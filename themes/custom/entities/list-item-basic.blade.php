9bb6ce7bc352a9828bc458b124948d01f973b5a7
Vuonglestudent <vuongvaba1v2@gmail.com>
2025-05-09 00:19:26 +0700

    custom css

--- themes/custom/entities/list-item-basic.blade.php
+++ themes/custom/entities/list-item-basic.blade.php
@@ -1,10 +1,18 @@
-<?php $type = $entity->getType(); ?>
-<a href="{{ $entity->getUrl() }}" class="{{$type}} {{$type === 'page' && $entity->draft ? 'draft' : ''}} {{$classes ?? ''}} entity-list-item" data-entity-type="{{$type}}" data-entity-id="{{$entity->id}}">
-    <div class="content">
-        <h4 class="entity-list-item-name break-text">
-            <span role="presentation" class="icon text-{{$type}} mr-xs" style="display: inline-block; vertical-align: middle;">@icon($type)</span>
-            {{ $entity->preview_name ?? $entity->name }}
-        </h4>
-        {{ $slot ?? '' }}
-    </div>
-</a>
+{{-- This file should not be loaded directly, but only included as a Blade component --}}
+@if(isset($entity))
+    <?php $type = $entity->getType(); ?>
+    <a href="{{ $entity->getUrl() }}" class="{{$type}} {{$type === 'page' && $entity->draft ? 'draft' : ''}} {{$classes ?? ''}} entity-list-item" data-entity-type="{{$type}}" data-entity-id="{{$entity->id}}">
+        <span role="presentation" class="icon text-{{$type}}">
+            @icon($type)    
+        </span>
+        <div class="content">
+            <h4 class="entity-list-item-name break-text">
+                <span class="entity-name-text">{{ $entity->preview_name ?? $entity->name }}</span>
+            </h4>
+            {{ $slot ?? '' }}
+        </div>
+    </a>
+@else
+    <!-- $entity variable is not available when the template is loaded directly -->
+    <p>This is a BookStack Blade template and should not be accessed directly.</p>
+@endif
